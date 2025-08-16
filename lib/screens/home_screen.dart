d1c5b56626d00712f93c75db74a35953b3edbb15
amr-mohamed-abdo <mamr80715@gmail.com>
2025-08-16 16:25:36 +0300

    create floating action buttom

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_notes_body_widget.dart';
-import 'package:notes_app/widgets/floating_action_buttom_widget.dart';
+import 'package:notes_app/widgets/details_for_floating_action_buttom.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,9 +8,20 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: CustomFloatingActionButtonWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return DetailsForFloatingActionButtom();
+            },
+          );
+        },
+        shape: CircleBorder(),
+        backgroundColor: Colors.green,
+        child: Icon(Icons.add),
+      ),
       body: MainNotesBodyWidget(),
     );
   }
 }
-
---
+++ lib/widgets/details_for_floating_action_buttom.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailsForFloatingActionButtom extends StatelessWidget {
+  const DetailsForFloatingActionButtom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.grey);
+  }
+}
